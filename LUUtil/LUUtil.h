1208c0ed812abdf14bfe6881e1278bb45a969358
鬼道 <xukai.xk@taobao.com>
2014-08-23 09:55:08 +0800

    init

---
+++ LUUtil/LUUtil.h
@@ -0,0 +1,15 @@
+//
+//  LUUtil.h
+//  LUUtil
+//
+//  Created by luics on 8/23/14.
+//  Copyright (c) 2014 luics.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LUUtil : NSObject
+
++(int) add:(int)a withB:(int)b;
+
+@end
---
+++ LUUtil/LUUtil.m
@@ -0,0 +1,17 @@
+//
+//  LUUtil.m
+//  LUUtil
+//
+//  Created by luics on 8/23/14.
+//  Copyright (c) 2014 luics.com. All rights reserved.
+//
+
+#import "LUUtil.h"
+
+@implementation LUUtil
+
++(int) add:(int)a withB:(int)b{
+    return a + b;
+}
+
+@end
